3dbd22d1e9f8ae33f03b8bf5b4b0ff6b7375caf6
Joe Fleming <w33ble@users.noreply.github.com>
2018-01-17 08:56:22 -0700

    Chore: add eslint and prettier (#6)
    * chore: add eslint and prettier setup
    
    * chore: add prettier config
    
    * chore: prettier the codebase
    
    * chore: add linting to test script

--- src/index.js
+++ src/index.js
@@ -89,13 +89,14 @@ function parseDateMath(mathString, time, roundUp) {
     unit = mathString.charAt(i++);
 
     // append additional characters in the unit
-    for(let j=i; j < len; j++) {
+    for (let j = i; j < len; j++) {
       let unitChar = mathString.charAt(i);
-      if ((/[a-z]/i).test(unitChar)) {
+      if (/[a-z]/i.test(unitChar)) {
         unit += unitChar;
         i++;
+      } else {
+        break;
       }
-      else break;
     }
 
     if (units.indexOf(unit) === -1) {
@@ -119,5 +120,5 @@ export default {
   parse: parse,
   units: Object.freeze(units),
   unitsAsc: Object.freeze(unitsAsc),
-  unitsDesc: Object.freeze(unitsDesc)
+  unitsDesc: Object.freeze(unitsDesc),
 };
